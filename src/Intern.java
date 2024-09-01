fc310e4f7dc6052e3586007d1d9de54d65f03d11
pugeaale <pugeaale@gmail.com>
2024-09-01 08:51:25 +0200

    :sparkles: adding intern class extending employee with salary limit

---
+++ src/Intern.java
@@ -0,0 +1,8 @@
+public class Intern extends  Employee {
+
+    private static final int SALARY_LIMIT = 20_000;
+
+    public Intern(String name, String email, int age, int salary) {
+        super(name, email, age, salary);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Employee employee1 = new Employee("salvatore", "salvatore.c@ironhack.com", 35, 205000 );
         System.out.println(employee1);
+        Intern intern1 = new Intern("giuseppe","g.corleone",18,18000);
+        System.out.println(intern1);
     }
 }
