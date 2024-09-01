fed41f5cc8ad43d15e429567c00bd4fec7b900d2
pugeaale <pugeaale@gmail.com>
2024-09-01 08:45:24 +0200

    :sparkles: adding employee class to represent an employee of an company

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Employee employee1 = new Employee("salvatore", "salvatore.c@ironhack.com", 35, 205000 );
+        System.out.println(employee1);
     }
 }
